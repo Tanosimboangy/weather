8cee641660df7fe29b0262e72a3f5de568077ef8
Tanosimboangy <jacquit.aja@onja.org>
2021-01-12 09:34:13 +0300

    trying to control the input in the form

--- Components/Search.js
+++ Components/Search.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Search({data}) {
+    // const [inputValue, setInputValue] = setState("");
+    const [inputValue, setInputValue] = useState('');
+    console.log(inputValue);
 
-    function showingSearchPage() {
-        console.log("I am clicked");
-    }
     function Searchitem(e) {
         e.preventDefault();
         console.log("I am clicked");
@@ -12,12 +12,25 @@ function Search({data}) {
 
     return (
         <div className="search_container">
-            <button onClick={showingSearchPage}>Search for places</button>
-            <button className="title">{data.title}</button>
-            <form onSubmit={Searchitem}>
-                <input type="text" required/>
-                <button type="submit">Search</button>
-            </form>
+            <button 
+                // onClick={showingSearchPage} 
+                className="search">
+                    Search for places
+            </button>
+            <div className="search_form">
+                <form onSubmit={Searchitem}>
+                    <input 
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue} 
+                        required/>
+                    <button 
+                        type="submit">
+                            Search
+                    </button>
+                </form>
+                <button className="title">{data.title}</button>
+            </div>
         </div>
     )
 }
--- dist/weather.e31bb0bc.js
+++ dist/weather.e31bb0bc.js
@@ -29780,16 +29780,18 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function Search({
   data
 }) {
-  function showingSearchPage() {
-    console.log("I am clicked");
-  }
+  // const [inputValue, setInputValue] = setState("");
+  const [inputValue, setInputValue] = (0, _react.useState)('');
+  console.log(inputValue);
 
   function Searchitem(e) {
     e.preventDefault();
@@ -29799,17 +29801,22 @@ function Search({
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "search_container"
   }, /*#__PURE__*/_react.default.createElement("button", {
-    onClick: showingSearchPage
-  }, "Search for places"), /*#__PURE__*/_react.default.createElement("button", {
-    className: "title"
-  }, data.title), /*#__PURE__*/_react.default.createElement("form", {
+    // onClick={showingSearchPage} 
+    className: "search"
+  }, "Search for places"), /*#__PURE__*/_react.default.createElement("div", {
+    className: "search_form"
+  }, /*#__PURE__*/_react.default.createElement("form", {
     onSubmit: Searchitem
   }, /*#__PURE__*/_react.default.createElement("input", {
     type: "text",
+    onChange: e => setInputValue(e.target.value),
+    value: inputValue,
     required: true
   }), /*#__PURE__*/_react.default.createElement("button", {
     type: "submit"
-  }, "Search")));
+  }, "Search")), /*#__PURE__*/_react.default.createElement("button", {
+    className: "title"
+  }, data.title)));
 }
 
 var _default = Search;
